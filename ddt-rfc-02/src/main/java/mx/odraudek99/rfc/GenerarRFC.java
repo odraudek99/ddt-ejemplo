ce06eefe6478882fe3486ed9944aecad6d2e0b08
odraudek99 <odraudek99>
2016-05-19 16:07:41 -0500

    ddt 2

---
+++ ddt-rfc-02/src/main/java/mx/odraudek99/rfc/GenerarRFC.java
@@ -0,0 +1,13 @@
+package mx.odraudek99.rfc;
+
+public class GenerarRFC {
+
+	
+	public String generarRFCAleatorio() {
+		
+		StringBuffer rfc = new StringBuffer();
+		
+		return rfc.toString();
+	}
+	
+}
